c88f6835dfa9eeb25981eb1a4981df31b821767f
Messiah <m.muzafarov@gmail.com>
2013-06-10 14:38:21 +0600

    templates

--- messiah/settings.py
+++ messiah/settings.py
@@ -116,12 +116,13 @@ MIDDLEWARE_CLASSES = (
     "django.contrib.messages.middleware.MessageMiddleware",
 )
 
-ROOT_URLCONF = 'messiah.urls'
+ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'messiah.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 TEMPLATE_DIRS = (
+    rel("templates"),
     # Put strings here, like "/home/html/django_templates"
     # or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
