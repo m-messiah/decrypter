077b8f5be9801f194c1441cf764257931712d5bd
Messiah <m.muzafarov@gmail.com>
2013-06-10 15:02:23 +0600

    static

--- messiah/settings.py
+++ messiah/settings.py
@@ -75,7 +75,9 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    rel("static")
+    rel("static"),
+    rel("..", "static"),
+    rel("messiah", "static"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
