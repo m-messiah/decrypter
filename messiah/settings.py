23d0eef470bfbedb48203abca1eb21644144d1b6
Messiah <m.muzafarov@gmail.com>
2013-06-10 15:05:27 +0600

    static

--- messiah/settings.py
+++ messiah/settings.py
@@ -67,7 +67,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = rel('../static/')
+STATIC_ROOT = rel('..', 'static/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -76,7 +76,6 @@ STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
     rel("static"),
-    rel("..", "static"),
     rel("messiah", "static"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
