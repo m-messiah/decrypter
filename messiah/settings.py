a156e85684fc936a3487a3b49155cd6e62cb6fcc
Messiah <m.muzafarov@gmail.com>
2013-06-10 22:08:40 +0600

    move

--- messiah/settings.py
+++ messiah/settings.py
@@ -29,7 +29,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["messiah.dlinkddns.com", "messiah-netbook", "localhost", "m-messiah.herokuapp.com"]
+ALLOWED_HOSTS = ["messiah.dlinkddns.com", "messiah-netbook", "localhost", "decrypter.herokuapp.com"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
