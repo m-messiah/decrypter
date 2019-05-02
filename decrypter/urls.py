fc4cc67eee10a9689e3c4a2197a46ad901f7d1a1
Maxim Muzafarov <m-messiah@yandex-team.ru>
2019-05-02 17:23:13 +0300

    Update Django==2.2.1

--- decrypter/urls.py
+++ decrypter/urls.py
@@ -1,20 +1,21 @@
-from django.conf.urls import patterns, url
+from django.urls import path
 from django.conf import settings
+from django.views import static
 from django.views.generic.base import TemplateView
 from django.views.generic import RedirectView
 
 from decrypter import views
 
 
-urlpatterns = patterns(
-    '',
-    url(r'^static/(?P<path>.*)$',
-        'django.views.static.serve',
-        {'document_root': settings.STATIC_ROOT}),
-    url(r'^$', views.decrypter),
-    url(r'^abc$', views.abc),
-    url(r'^robots\.txt$',
+urlpatterns = [
+    path(r'static/<path:path>',
+         static.serve, kwargs={'document_root': settings.STATIC_ROOT},
+         name='static'),
+    path(r'', views.decrypter, name='main'),
+    path(r'abc', views.abc, name='abc'),
+    path('robots.txt',
         TemplateView.as_view(template_name='robots.txt',
-                             content_type='text/plain')),
-    url(r'^favicon\.png$', RedirectView.as_view(url='/static/favicon.png')),
-)
+                             content_type='text/plain'),
+        name='robots'),
+    path('favicon.png', RedirectView.as_view(url='/static/favicon.png'), name='favicon'),
+]
